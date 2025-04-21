4b5d4f785db376b90797ca057051c23574cd921b
Saikrishna2525 <saikrishna.robotics.25@gmail.com>
2025-04-21 12:20:20 +0530

    Add files via upload

---
+++ String/NoOfChar.py
@@ -0,0 +1,9 @@
+string = input("Enter Your String: ").lower()
+frequency = {}
+for i in string:
+    if i not in frequency:
+        if i != "\t":
+            frequency[i] = string.count(i)
+        elif i == "\t":
+            frequency["Tab"] = string.count("\t")
+print(frequency)
