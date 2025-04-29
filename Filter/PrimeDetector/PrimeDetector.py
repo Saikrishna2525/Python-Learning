ba40acc1e247bd195fd8d13d12b7c0a97159a3be
Sai <saikrishna.robotics.25@gmail.com>
2025-04-29 15:59:30 +0530

    Filter Functions Added

---
+++ Filter/PrimeDetector/PrimeDetector.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 20]
+prime_numbers = list(filter(is_prime, numbers))
+
+print(prime_numbers)
